57cb3c7f3266d4530881550a1530e8aabc53add8
mattRepos <matveyderudjov.gmail.com>
2024-10-14 18:18:21 +0300

    The main page is ready, pages index and about

--- main/views.py
+++ main/views.py
@@ -4,15 +4,18 @@ from django.http import HttpResponse
 
 def index(request):
     context = {
-        'title': 'home',
-        'content': 'main page store -- HOME',
-        'list': ['first', 'second'],
-        'dict': {'first': 1},
-        'is_authenticated': False
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME'
     }
 
     return render(request, 'main/index.html', context)
 
 
 def about(request):
-    return HttpResponse('ABOUT PAGE')
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Пример какого то текста'
+    }
+
+    return render(request, 'main/about.html', context)
